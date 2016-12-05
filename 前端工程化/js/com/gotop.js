20f31b7176669d1a1b2a142dd624988256716a9b
xuxiaokun <996670910@qq.com>
2016-12-05 16:35:40 +0800

    运用 requirejs & r.js 实现前端工程化

---
+++ 前端工程化/js/com/gotop.js
@@ -0,0 +1,22 @@
+define(['jquery'], function($){
+  var $goTop = $('<div class="go-top">回到顶部</div>');
+  $('body').append($goTop);
+  $goTop.hide();
+
+  function init(){
+    $(window).on('scroll', function(){
+      var offsetTop = $('body').scrollTop();
+      if(offsetTop > 100){
+        $goTop.show();
+      }else {
+        $goTop.hide();
+      }
+    });
+    $goTop.on('click', function(){
+      $(window).scrollTop(0);
+    });
+  }
+  return {
+    init: init
+  }
+});
